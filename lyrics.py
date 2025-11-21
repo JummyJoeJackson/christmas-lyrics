18b97e54a2e3774c94ca1ef380848fc3a48262df
JummyJoeJackson <diegogonaguilar@gmail.com>
2025-11-21 16:34:23 -0500

    Made tree and colours constants

--- lyrics.py
+++ lyrics.py
@@ -3,26 +3,26 @@ import random
 import time
 from colorama import Fore
 
+TREE = ["\n       *         ",
+        "      ***        ",
+        "     *****       ",
+        "    *******      ",
+        "   *********     ",
+        "  ***********    ",
+        " *************   ",
+        "***************  ",
+        "      |||        ",
+        "     /|||\\       "]
+
+COLORS = [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.YELLOW, Fore.MAGENTA, Fore.GREEN, Fore.GREEN]
+
 
 def print_tree():
-    tree = ["\n       *         ",
-            "      ***        ",
-            "     *****       ",
-            "    *******      ",
-            "   *********     ",
-            "  ***********    ",
-            " *************   ",
-            "***************  ",
-            "      |||        ",
-            "     /|||\\       "]
-
-    colors = [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.YELLOW, Fore.MAGENTA, Fore.GREEN, Fore.GREEN]
-
-    print(colors[3] + tree[0])
-    for row in tree[1:]:
+    print(COLORS[3] + TREE[0])
+    for row in TREE[1:]:
         for char in row:
             if char == "*":
-                color = random.choice(colors)
+                color = random.choice(COLORS)
                 print(color + char, end="", flush=True)
             else:
                 print(Fore.WHITE + char, end="", flush=True)
